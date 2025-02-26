a317968fd6ad329f8bf9a908b1180c01042e321f
Bharat Gungoman <bharatgungoman2020@gmail.com>
2025-02-26 13:07:16 +0530

    selelct-mood-feature-add

--- chat/views.py
+++ chat/views.py
@@ -8,7 +8,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-# Create your views here.
+
 
 def login_page(request):
     if request.method == "POST":
--- moodcheck/settings.py
+++ moodcheck/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+from django.contrib import admin
+from django.urls import path, include
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
